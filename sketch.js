538e15df929a4c50f73d2d55b7c9b6c192d0edd5
future-interactions <matt@future-interactions.com>
2022-06-14 18:55:33 +0100

    added hit test for swatch

--- sketch.js
+++ sketch.js
@@ -4,7 +4,11 @@ var numSwatches = 12;
 var pickedCol;
 let mosImage, swatches;
 let swatchesCol = ["#d47777", "#d4d077", "#bdd477", "#77d4a1", "#77c9d4", "#779ed4", "#e2bbbb", "#dce2bb", "#7d5e63", "#7d765e", "#d3a709", "#6d7368"]
-
+let stl;
+let str;
+let sbl;
+let sbr;
+let swatchCenter;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -12,6 +16,13 @@ function setup() {
 	console.log("swatchesCol length = " + swatchesCol.length);
 	mosImage = createGraphics(vMosaicX, vMosaicX);
 	swatches = createGraphics(numSwatches / 6 * vScale * 2, numSwatches / 2 * vScale * 2);
+
+	swatchCenter = createVector(width / 2 - vMosaicX / 2 - (vScale * 4), height / 2 - vMosaicX / 4);//swatch center
+	stl = createVector(width / 2 - vMosaicX / 2 - (vScale * 6), height / 2 - vMosaicX / 2); //swatch top left
+	str = createVector(width / 2 - vMosaicX / 2 - (vScale * 2), height / 2 - vMosaicX / 2); //swatch top right
+	sbl = createVector(width / 2 - vMosaicX / 2 - (vScale * 6), height / 2); //swatch bottom left
+	sbr = createVector(width / 2 - vMosaicX / 2 - (vScale * 2), height / 2); //swatch bottom right
+
 }
 
 function draw() {
@@ -35,9 +46,22 @@ function draw() {
 	}
 	image(mosImage, width / 2 - vMosaicX / 2, height / 2 - vMosaicX / 2);
 	image(swatches, width / 2 - vMosaicX / 2 - (vScale * 6), height / 2 - vMosaicX / 2);
-	if (mouseIsPressed === true) {
-		pickedCol=get(mouseX, mouseY);
-		console.log(pickedCol);
+
+
+
+
+	/*ellipse(stl.x, stl.y, 10);
+	ellipse(str.x, str.y, 10);
+	ellipse(sbl.x, sbl.y, 10);
+	ellipse(sbr.x, sbr.y, 10);
+	ellipse(swatchCenter.x, swatchCenter.y, 10);
+	*/
+
+
+	if (mouseIsPressed === true && mouseX>stl.x &&mouseX<str.x&&mouseY>stl.y&&mouseY<sbr.y) {
+		pickedCol = get(mouseX, mouseY);
+		ellipse(mouseX, mouseY, 20,20);
+		//console.log(pickedCol);
 	}
 }
 
