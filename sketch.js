314e0063fe781a0ca0737cdd87195f90cdbffe5c
future-interactions <matt@future-interactions.com>
2022-06-16 17:25:50 +0100

    added scaling on arrow up and arrow down

--- sketch.js
+++ sketch.js
@@ -1,45 +1,37 @@
 var vMosaicX = 500;
-var vScale = 25;
-var numSwatches = 12; //we only have 12 colors in swatch col list so if this changes then update the colors too
-var pickedCol = "#eeeeee";
+var numTiles = 30;
+var vScale;
+const numSwatches = 12; //we only have 12 colors in swatch col list so if this changes then update the colors too
+var pickedCol = "#ffffff";
 let mosImage, swatches;
-let swatchesCol = ["#ffffff", "#d47777", "#bdd477", "#77d4a1", "#77c9d4", "#779ed4", "#e2bbbb", "#dce2bb", "#7d5e63", "#7d765e", "#d3a709", "#6d7368"]
+var loc = 0;
+const swatchesCol = ["#ffffff",
+	"#d47777",
+	"#bdd477",
+	"#77d4a1",
+	"#77c9d4",
+	"#779ed4",
+	"#e2bbbb",
+	"#dce2bb",
+	"#7d5e63",
+	"#7d765e",
+	"#d3a709",
+	"#6d7368"
+]
+
 let stl, sbr, str, swatchCenter; //swatch variables for cordinates
 let ctl, cbr, canvasCenter; //canvas variables for cordinates
 var clickedVal = false;
-var counter = 0; //this is just for testing
 var currentCellId;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	vScale = round(vMosaicX / numTiles);
 	pixelDensity(1);
-	console.log("swatchesCol length = " + swatchesCol.length);
 	mosImage = createGraphics(vMosaicX, vMosaicX);
-	swatches = createGraphics(numSwatches / 6 * vScale * 2, numSwatches / 2 * vScale * 2);
-
-	swatchCenter = createVector(width / 2 - vMosaicX / 2 - (vScale * 4), height / 2 - vMosaicX / 4); //swatch center
-	stl = createVector(width / 2 - vMosaicX / 2 - (vScale * 6), height / 2 - vMosaicX / 2); //swatch top left
-	sbr = createVector(width / 2 - vMosaicX / 2 - (vScale * 2), height / 2 - vMosaicX / 2 + (vScale * 6 * 2)); //swatch bottom right
-	str = createVector(width / 2 - vMosaicX / 2 - (vScale * 2), height / 2 - vMosaicX / 2); //swatch top right
-	ctl = createVector(width / 2 - vMosaicX / 2, height / 2 - vMosaicX / 2); //canvas top left
-	cbr = createVector(width / 2 + vMosaicX / 2, height / 2 + vMosaicX / 2); //canvas bottom right
-	for (let x = 0; x < vMosaicX; x += vScale) {
-		for (let y = 0; y < vMosaicX; y += vScale) {
-			mosImage.stroke(230, 230, 220);
-			mosImage.fill("#eeeeee");
-			mosImage.rect(x, y, vScale);
-		}
-	}
-	let loc = 0;
-	for (let b = 0; b < numSwatches / 2 * vScale * 2; b += vScale * 2) {
-		for (let a = 0; a < numSwatches / 6 * vScale * 2; a += vScale * 2) {
-			loc++;
-			//console.log(loc);
-			swatches.fill(swatchesCol[loc - 1]);
-			swatches.stroke(230, 230, 220);
-			swatches.rect(a, b, vScale * 2);
-		}
-	}
+	swatches = createGraphics(windowWidth, windowHeight);
+	swatches.clear();
+	drawMosaic();
 }
 
 function draw() {
@@ -47,9 +39,10 @@ function draw() {
 	stroke(180, 180, 160);
 	noFill();
 
-	rect(width / 2 - vMosaicX / 2 - vScale / 2, height / 2 - vMosaicX / 2 - vScale / 2, vMosaicX + vScale, vMosaicX + vScale)
+	image(swatches, width / 2 - vMosaicX / 2 - vScale * 6, height / 2 - vMosaicX / 2);
 	image(mosImage, width / 2 - vMosaicX / 2, height / 2 - vMosaicX / 2);
-	image(swatches, width / 2 - vMosaicX / 2 - (vScale * 6), height / 2 - vMosaicX / 2);
+	rect(width / 2 - vMosaicX / 2 - vScale / 2, height / 2 - vMosaicX / 2 - vScale / 2, vMosaicX + vScale, vMosaicX + vScale);
+
 
 	if (clickedVal == true) {
 		stroke(200);
@@ -102,17 +95,55 @@ function mousePressed() { //testing for within swatch panel
 
 function keyPressed() {
 	if (keyCode === BACKSPACE) {
-		for (let x = 0; x < vMosaicX; x += vScale) {
-			for (let y = 0; y < vMosaicX; y += vScale) {
-				mosImage.stroke(230, 230, 220);
-				mosImage.fill("#eeeeee");
-				mosImage.rect(x, y, vScale);
-			}
+		drawMosaic();
+	}
+	if (keyCode === DOWN_ARROW) {
+		if (numTiles < 90) {
+			numTiles += 10;
+		}
+		vScale = round(vMosaicX / numTiles);
+		drawMosaic();
+	}
+
+	if (keyCode === UP_ARROW) {
+		if (numTiles > 10) {
+			numTiles -= 10;
+		}
+		vScale = round(vMosaicX / numTiles);
+		drawMosaic();
+		console.log(numTiles);
+	}
+}
+
+function drawMosaic() {
+	swatches.background(220, 220, 210);
+
+	for (let b = 0; b < vScale * 6 * 2; b += vScale * 2) {
+		for (let a = 0; a < vScale * 2 * 2; a += vScale * 2) {
+			loc++;
+			swatches.fill(swatchesCol[loc - 1]);
+			//swatches.fill(loc*20, 0,0)
+			swatches.stroke(230, 230, 220);
+			swatches.rect(a, b, vScale * 2);
+		}
+		if (loc == 12) {
+			sbr = createVector(width / 2 - 250 - vScale * 2, height / 2);
 		}
 	}
 
+	loc = 0;
+	vScale = round(vMosaicX / numTiles);
+	for (let x = 0; x < vMosaicX; x += vScale) {
+		for (let y = 0; y < vMosaicX; y += vScale) {
+			mosImage.stroke(230, 230, 220);
+			mosImage.fill("#eeeeee");
+			mosImage.rect(x, y, vScale);
+		}
+	}
+	swatchCenter = createVector(width / 2 - vMosaicX / 2 - (vScale * 4), height / 2 - vMosaicX / 4); //swatch center
+	stl = createVector(width / 2 - vMosaicX / 2 - floor(vScale * 6), height / 2 - vMosaicX / 2); //swatch top left
+	sbr = createVector(width / 2 - 250 - vScale * 2, height / 2 - vMosaicX / 2 + vScale * 6 * 2); //swatch bottom right
+	str = createVector(width / 2 - vMosaicX / 2 - floor(vScale * 2), height / 2 - vMosaicX / 2); //swatch top right
+	ctl = createVector(width / 2 - vMosaicX / 2, height / 2 - vMosaicX / 2); //canvas top left
+	cbr = createVector(width / 2 + vMosaicX / 2, height / 2 + vMosaicX / 2); //canvas bottom right
 }
-
-
-
-
